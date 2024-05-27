666ddf4eb7e728c8f95b52490f68866792a81dab
websites <websites.unpinned074@passinbox.com>
2024-05-27 16:17:41 -0500

    Adding Items Implement

--- web.py
+++ web.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 todos = functions.get_todos()
 
 st.title('To-Do Webpage')
@@ -10,4 +16,5 @@ st.write("This app helps you stay organized!")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input("", placeholder="Enter a task...")
+st.text_input(label="", placeholder="Enter a task...",
+              on_change=add_todo, key='new_todo')
