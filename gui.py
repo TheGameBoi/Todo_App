a2b39695fa865b632e4be6b3198e5cf3f99e2032
websites <websites.unpinned074@passinbox.com>
2024-05-27 10:02:02 -0500

    Add Button Part 1

--- gui.py
+++ gui.py
@@ -1,3 +1,10 @@
-import FreeSimpleGUI
+import FreeSimpleGUI as sg
 
-window = FreeSimpleGUI.Window('To-Do List', layout=[""])
+text = sg.Text("Enter a To-Do")
+input_box = sg.InputText(tooltip="(Walk, Run, etc.)")
+add_button = sg.Button("Add")
+remove_button = sg.Button("Complete")
+edit_button = sg.Button("Edit")
+window = sg.Window('To-Do List', layout=[[text], [input_box], [add_button, edit_button, remove_button]])
+window.read()
+window.close()
